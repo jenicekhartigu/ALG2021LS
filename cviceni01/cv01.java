b313c44ce060ce4902f50c58a71974147bf4c16b
jenicekhartigu <honza.hartig@seznam.cz>
2021-02-26 09:50:38 +0100

    dalsi commit

--- cviceni01/cv01.java
+++ cviceni01/cv01.java
@@ -3,7 +3,7 @@ package cviceni01;
 public class cv01 {
     public static void main(String[] args)
     {
-        System.out.println("Hello ITnetwork!");
+        System.out.println("Hello dasdasda!");
     }
 
 }
---
+++ cviceni02/cv02.java
@@ -0,0 +1,8 @@
+package cviceni02;
+
+public class cv02 {
+    public static void main(String[] args)
+    {
+        System.out.println("hahsfkajhshfaskfla");
+    }
+}
