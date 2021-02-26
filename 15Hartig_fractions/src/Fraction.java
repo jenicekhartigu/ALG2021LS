44d8550c67db83b702eb9cc8493bda21a49bc563
jenicekhartigu <honza.hartig@seznam.cz>
2021-02-26 12:01:12 +0100

    FractionApp

---
+++ 15Hartig_fractions/src/Fraction.java
@@ -0,0 +1,26 @@
+public class Fraction {
+    public float citatel;
+    public float jmenovatel;
+
+    public Fraction(float aCitatel, float aJmenovatel)
+    {
+        setCitatel(aCitatel);
+        setJmenovatel(aJmenovatel);
+    }
+
+    public void setCitatel(float citatel) {
+        this.citatel = citatel;
+    }
+    
+    public void setJmenovatel(float jmenovatel) {
+        this.jmenovatel = jmenovatel;
+    }
+
+    public float getCitatel() {
+        return citatel;
+    }
+
+    public float getJmenovatel() {
+        return jmenovatel;
+    }
+}
---
+++ 15Hartig_fractions/src/FractionApp.java
@@ -0,0 +1,11 @@
+public class FractionApp {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello dasdasda!");
+        Fraction prvniZlomek;
+        prvniZlomek = new Fraction(10, 20);
+
+        System.out.println(prvniZlomek.getCitatel());
+        
+        System.out.println(prvniZlomek.getJmenovatel());
+    }
+}
