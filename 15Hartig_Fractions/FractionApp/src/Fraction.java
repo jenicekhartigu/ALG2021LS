28522a68c63b8e2c100d07087ddd997168991fd5
jenicekhartigu <honza.hartig@seznam.cz>
2021-02-26 16:40:02 +0100

    FractionApp - toString, getValue, prntFrac, prntFracNice

--- 15Hartig_Fractions/FractionApp/src/Fraction.java
+++ 15Hartig_Fractions/FractionApp/src/Fraction.java
@@ -1,40 +1,74 @@
 public class Fraction {
-    private float citatel;
-    private float jmenovatel;
+    private double citatel;
+    private double jmenovatel;
 
-    public Fraction(float aCitatel, float aJmenovatel)
-    {
-        setCitatel(aCitatel);
-        setJmenovatel(aJmenovatel);
+    // CONSTRUCTOR
+    // --------------------------------------------------------------------------------
+    public Fraction() {
+        setCit(0);
+        setJme(0);
     }
 
-    public void setCitatel(float citatel) {
+    public Fraction(double aCitatel, double aJmenovatel) {
+        setCit(aCitatel);
+        setJme(aJmenovatel);
+    }
+
+    // GETTERY SETTERY
+    // ----------------------------------------------------------------------------
+    public void setCit(double citatel) {
         this.citatel = citatel;
     }
-    
-    public void setJmenovatel(float jmenovatel) {
+
+    public void setJme(double jmenovatel) {
         this.jmenovatel = jmenovatel;
     }
 
-    public float getCitatel() {
+    public double getCit() {
         return citatel;
     }
 
-    public float getJmenovatel() {
+    public double getJme() {
         return jmenovatel;
     }
 
-    public float getVysledek() {
-        float vysledek;
-        vysledek = citatel/jmenovatel;
-        return vysledek;
+    // FUNKCE
+    // -------------------------------------------------------------------------------------
+
+    public String isValid() {
+        if (this.getJme() == 0) {
+            return "\n- Nefunkcni zlomek, nelze delit nulou\n";
+        }
+        return "\n";
     }
 
-    public String vypisZlomek() {
-        return citatel +"/" +jmenovatel;
+    public String toString() {
+        return this.getCit() + " " + this.getJme() + this.isValid();
     }
 
-    public String vypisZlomekHezky() {
-        return citatel +"\n------\n" +jmenovatel;
+    public double getValue() {
+        double res;
+        if (this.getJme() == 0) {
+            return 0;
+        }
+        res = this.getCit() / this.getJme();
+        return res;
+    }
+
+    public String prntFrac() {
+        return this.getCit() + "/" + this.getJme() + this.isValid();
+    }
+
+    public String prntFracNice() {
+        String cit = String.valueOf(this.getCit());
+        String jme = String.valueOf(this.getJme());
+        int lenCit = cit.length();
+        int lenJme = jme.length();
+        int divLineLen = Math.max(lenCit, lenJme);
+        String divLine = "";
+        for (int i = 0; i < divLineLen; i++) {
+            divLine += "_";
+        }
+        return this.getCit() + "\n" + divLine + "\n" + this.getJme() + this.isValid();
     }
 }
--- 15Hartig_Fractions/FractionApp/src/FractionApp.java
+++ 15Hartig_Fractions/FractionApp/src/FractionApp.java
@@ -1,16 +1,24 @@
 public class FractionApp {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello dasdasda!");
-        Fraction prvniZlomek = new Fraction(10, 20);
-        Fraction druhyZlomek = new Fraction(456, 13);
+        Fraction zlom1 = new Fraction(100, 25);
+        Fraction zlom2 = new Fraction(4555, 0);
 
-        System.out.println(prvniZlomek.getCitatel());
-        System.out.println(prvniZlomek.getJmenovatel());
+        System.out.println("citatel zl1: " + zlom1.getCit());
+        System.out.println("jmenovatel zl1: " + zlom1.getJme());
         System.out.println();
-        System.out.println(prvniZlomek.vypisZlomek());
-        System.out.println(prvniZlomek.getVysledek());
-        System.out.println();
-        System.out.println(druhyZlomek.vypisZlomekHezky());
-        System.out.println(druhyZlomek.getVysledek());
+
+        System.out.println("to string zl1: " + zlom1.toString());
+        System.out.println("to string zl2: " + zlom2.toString());
+
+        System.out.println("Dva druhy vypisu zl1");
+        System.out.println(zlom1.prntFrac());
+        System.out.println(zlom1.prntFracNice());
+
+        System.out.println("Dva druhy vypisu zl2");
+        System.out.println(zlom2.prntFrac());
+        System.out.println(zlom2.prntFracNice());
+
+        System.out.println("value zl1: " + zlom1.getValue());
+        System.out.println("value zl2: " +zlom2.getValue());
     }
 }
