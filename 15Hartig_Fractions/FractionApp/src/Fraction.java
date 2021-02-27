fe7263137d06111605acee214132b600868a676c
jenicekhartigu <honza.hartig@seznam.cz>
2021-02-27 22:04:38 +0100

    FractionApp - Komentáře, nsd, nsdValue

--- 15Hartig_Fractions/FractionApp/src/Fraction.java
+++ 15Hartig_Fractions/FractionApp/src/Fraction.java
@@ -16,18 +16,34 @@ public class Fraction {
 
     // GETTERY SETTERY
     // ----------------------------------------------------------------------------
+    /**
+     * Setter pro hodnotu citatel<p>
+     * nastavení hodnoty citatel
+     */
     public void setCit(double citatel) {
         this.citatel = citatel;
     }
 
+    /**
+     * Setter pro hodnotu jmenovatel<p>
+     * nastavení hodnoty jmenovatel
+     */
     public void setJme(double jmenovatel) {
         this.jmenovatel = jmenovatel;
     }
 
+    /**
+     * Getter pro hodnotu citatel<p>
+     * @return citatel v datovém typu Double
+     */
     public double getCit() {
         return citatel;
     }
 
+    /**
+     * Getter pro hodnotu jmenovatel<p>
+     * @return jmenovatel v datovém typu Double
+     */
     public double getJme() {
         return jmenovatel;
     }
@@ -35,31 +51,88 @@ public class Fraction {
     // FUNKCE
     // -------------------------------------------------------------------------------------
 
-    public String isValid() {
+    private void isValid() {
         if (this.getJme() == 0) {
-            return "\n- Nefunkcni zlomek, nelze delit nulou\n";
+            throw new IllegalArgumentException("\nNeplatny zlomek, deleni nulou\n" + getCit() + "/" + getJme() + " <- NULA\n");
         }
-        return "\n";
     }
 
+    /**
+     * Metoda pro vracení zlomku
+     * <p>
+     * Pro výpis:
+     * <blockquote>
+     * <pre>
+     * System.out.printf("%s", this.getValue());
+     * </pre>
+     * </blockquote>
+     * 
+     * @return zlomek v datovém typu String
+     */
     public String toString() {
-        return this.getCit() + " " + this.getJme() + this.isValid();
+        this.isValid();
+        return this.getCit() + " " + this.getJme();
     }
 
+    /**
+     * Metoda pro vracení hodnoty zlomku
+     * <p>
+     * Pro vypis bez zaokrouhlení:
+     * <blockquote>
+     * <pre>
+     * System.out.printf("%f", this.getValue());
+     * </pre>
+     * </blockquote>
+     * Pro vypis se zaokrouhlením:
+     * <blockquote>
+     * <pre>
+     * System.out.printf("%.2f", this.getValue());
+     * </pre>
+     * </blockquote>
+     * @return hodnota zlomku v datovém typu Double
+     */
     public double getValue() {
+        this.isValid();
         double res;
-        if (this.getJme() == 0) {
-            return 0;
-        }
         res = this.getCit() / this.getJme();
         return res;
+
+        
     }
 
+    /**
+     * Metoda pro vrácení zlomku<p>
+     * Pro výpis:
+     * <blockquote>
+     * <pre>
+     * System.out.printf("%s", this.prntFrac());
+     * </pre>
+     * </blockquote>
+     * 
+     * @return zlomek v datovém typu String<p>
+     * citatel / jmenovatel
+     */
     public String prntFrac() {
-        return this.getCit() + "/" + this.getJme() + this.isValid();
+        this.isValid();
+        return this.getCit() + "/" + this.getJme();
     }
 
+    /**
+     * Metoda pro vrácení zlomku<p>
+     * Pro výpis:
+     * <blockquote>
+     * <pre>
+     * System.out.printf("%s", this.prntFracNice());
+     * </pre>
+     * </blockquote>
+     * 
+     * @return zlomek v datovém typu String<p>
+     * citatel<p>
+     * __________<p>
+     * jmenovatel
+     */
     public String prntFracNice() {
+        this.isValid();
         String cit = String.valueOf(this.getCit());
         String jme = String.valueOf(this.getJme());
         int lenCit = cit.length();
@@ -69,6 +142,67 @@ public class Fraction {
         for (int i = 0; i < divLineLen; i++) {
             divLine += "_";
         }
-        return this.getCit() + "\n" + divLine + "\n" + this.getJme() + this.isValid();
+        return this.getCit() + "\n" + divLine + "\n" + this.getJme();
+    }
+
+    /**
+     * 
+     * Metoda pro zjištění NSD<p>
+     * NSD - Nejmenší společný dělitel<p>
+     * Pro výpis:
+     * <blockquote>
+     * <pre>
+     * System.out.printf("%s", this.nsd());
+     * </pre>
+     * </blockquote>
+     * 
+     * @return NSD v datovém typu String<p>
+     */
+    public String nsd() {
+        this.isValid();
+        double a = this.getCit();
+        double b = this.getJme();
+        if (a < 1 || b < 1)
+            throw new IllegalArgumentException("Citatel nebo jmenovatel je mensi nez 1");
+        while (b != 0) {
+            double tmp = a;
+            a = b;
+            b = tmp % b;
+        }
+        if (a == 1) {
+            return "Zlomek nema NSD";
+        }
+        return "NSD = " + a;
+    }
+
+    /**
+     * Metoda pro zjištění NSD a vracení<p>
+     * NSD - Nejmenší společný dělitel<p>
+     * Pro výpis:
+     * <blockquote>
+     * <pre>
+     * System.out.printf("%f", this.nsdValue());
+     * </pre>
+     * </blockquote>
+     * <p>
+     * Pokud zlomek nemá NSD, metoda vrací 0.
+     * 
+     * @return NSD v datovém typu Double<p>
+     */
+    public double nsdValue() {
+        this.isValid();
+        double a = this.getCit();
+        double b = this.getJme();
+        if (a < 1 || b < 1)
+            throw new IllegalArgumentException("Citatel nebo jmenovatel je mensi nez 1");
+        while (b != 0) {
+            double tmp = a;
+            a = b;
+            b = tmp % b;
+        }
+        if (a == 1) {
+            return 0;
+        }
+        return a;
     }
 }
--- 15Hartig_Fractions/FractionApp/src/FractionApp.java
+++ 15Hartig_Fractions/FractionApp/src/FractionApp.java
@@ -1,24 +1,16 @@
 public class FractionApp {
     public static void main(String[] args) throws Exception {
-        Fraction zlom1 = new Fraction(100, 25);
-        Fraction zlom2 = new Fraction(4555, 0);
-
-        System.out.println("citatel zl1: " + zlom1.getCit());
-        System.out.println("jmenovatel zl1: " + zlom1.getJme());
-        System.out.println();
-
-        System.out.println("to string zl1: " + zlom1.toString());
-        System.out.println("to string zl2: " + zlom2.toString());
-
-        System.out.println("Dva druhy vypisu zl1");
-        System.out.println(zlom1.prntFrac());
-        System.out.println(zlom1.prntFracNice());
-
-        System.out.println("Dva druhy vypisu zl2");
-        System.out.println(zlom2.prntFrac());
-        System.out.println(zlom2.prntFracNice());
-
-        System.out.println("value zl1: " + zlom1.getValue());
-        System.out.println("value zl2: " +zlom2.getValue());
+        Fraction zlom1 = new Fraction(300, 216);
+        Fraction zlom2 = new Fraction(6, 0);
+        
+        System.out.printf("%.2f\n",zlom1.getCit());
+        System.out.printf("%.2f\n",zlom1.getJme());
+        System.out.printf("%s\n",zlom1.toString());
+        System.out.printf("%s\n",zlom1.prntFrac());
+        System.out.printf("%s\n",zlom1.prntFracNice());
+        System.out.printf("%.2f\n",zlom1.getValue());
+        System.out.printf("%s\n",zlom1.nsd());
+        System.out.printf("%s\n",zlom1.nsdValue());
+        System.out.printf("%f",zlom2.getValue());
     }
 }
