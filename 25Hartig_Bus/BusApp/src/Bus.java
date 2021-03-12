37470aa73aa827317a032532ab7b3f3e163d56bb
jenicekhartigu <honza.hartig@seznam.cz>
2021-03-12 11:11:05 +0100

    cv02-setter, validace

--- 25Hartig_Bus/BusApp/src/Bus.java
+++ 25Hartig_Bus/BusApp/src/Bus.java
@@ -1,3 +1,51 @@
 public class Bus {
+    private String nazSpol;
+    private int cisLinky;
+    private int pocSed;
+    private int pocCes;
+
+    public Bus() {
+        nazSpol = "Bez společnosti";
+        cisLinky = 0;
+        pocSed = 40;
+        pocCes = 0;
+    }
+
+    public Bus(String nazSpol, int cisLinky, int pocSed, int pocCes) {
+        this.nazSpol = nazSpol;
+        this.cisLinky = cisLinky;
+        jeDostDedadel(pocSed, pocCes);
+        this.pocSed = pocSed;
+        this.pocCes = pocCes;
+    }
+    
+    private void jeDostDedadel(int pocSed, int pocCes) {
+        if (pocCes > pocSed){
+            throw new IllegalArgumentException("\n Pocet cestujicich je vetsi nez pocet sedadel\n");
+        }
+    }
+
+    
+
+
+    public String toString() {
+        return "Autobus cislo "+ cisLinky +" spolecnosti "+ nazSpol +" s poctem sedadel "+ pocSed +" veze "+ pocCes +" cestujicich \n";
+    }
+
+    public void setNazSpol(String nazSpol) {
+        this.nazSpol = nazSpol;
+    }
+
+    public void setCisLinky(int cisLinky) {
+        this.cisLinky = cisLinky;
+    }
+
+    /*
+    public void vypisBus() {
+        System.out.printf("Autobus cislo %d spolecnosti %s s poctem sedadel %d veze %d cestujicich \n", cisLinky, nazSpol, pocSed, pocCes);
+    }
+    */
+
+
     
 }
--- 25Hartig_Bus/BusApp/src/BusApp.java
+++ 25Hartig_Bus/BusApp/src/BusApp.java
@@ -1,5 +1,15 @@
 public class BusApp {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Bus mujBus = new Bus("Arriva", 69, 40, 20);
+        mujBus.setNazSpol("BusLine");
+        mujBus.setCisLinky(98);
+
+        System.out.println(mujBus.toString());
+
+        Bus mujBus2 = new Bus("Arriva", 69, 20, 40);
+
+        
     }
 }
